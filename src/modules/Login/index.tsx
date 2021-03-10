bc2b14ad9ceb0439c158a9ee62a9c0c405f74b3f
Cuong DC <cuongdc@mcg-corp.vn>
2021-03-10 22:33:35 +0700

    no message

--- src/modules/Login/index.tsx
+++ src/modules/Login/index.tsx
@@ -1,7 +1,61 @@
 import styled from "styled-components";
 import logo from "../../assets/login/Image.png";
-import title from "../../assets/login/title.png"
+import title from "../../assets/login/title.png";
+import login from "../../assets/login/i-login.png";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+} from "react-router-dom";
 
+import Dashboard from "../Dashboard/index"
+
+const index = () =>{
+    return(
+        <Router>
+            <All>
+                <Body>
+                    <BodyLeft>
+                        <div style={{position:"absolute",bottom:148,right:0}}>
+                            <DivInput>
+                                <TitleInput>Email</TitleInput>
+                                <Input/>
+                            </DivInput>
+                            <DivInput>
+                                <TitleInput>PASSWORD</TitleInput>
+                                <Input onChange={()=>console.log("1")}/>
+                            </DivInput>
+                        </div>
+
+                        <BtnLogin src={login}>
+
+
+                        </BtnLogin>
+                        <Link to = "/Dashboard">aaaaa</Link>
+
+
+
+                    </BodyLeft>
+                    <div style={{position:"relative"}}>
+                        <Image src={logo}/>
+                        <ImageTitle src={title}/>
+                    </div>
+
+                </Body>
+
+            </All>
+            <Switch>
+                <Route path="/Dashboard">
+                    <Dashboard />
+                </Route>
+            </Switch>
+        </Router>
+
+    )
+}
 const All = styled.div`
   top: 0px;
   left: 0px;
@@ -19,6 +73,7 @@ const BodyLeft = styled.div`
   background: #FFFFFF 0% 0% no-repeat padding-box;
   box-shadow: 0px 3px 80px #0000001A;
   opacity: 1;
+  position: relative;
 `
 const Image = styled.img`
     height: 860px;
@@ -33,19 +88,42 @@ const ImageTitle = styled.img`
     height: 340px;
   width: 512px;
   position: absolute;
+  left: -256px;
+  top: 200px;
+  
 `
-const index = () =>{
-    return(
-    <All>
-    <Body>
-      <BodyLeft>
 
-      </BodyLeft>
-        <Image src={logo}/>
-        <ImageTitle src={title}/>
-    </Body>
-    </All>
-    )
-}
-
+const DivInput = styled.div`
+    width: 303px;
+    height: 80px;
+    background: #FFFFFF 0% 0% no-repeat padding-box;
+border: 1px solid #D8D8D8;
+opacity: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  `
+const TitleInput = styled.text`
+  text-align: left;
+  font: normal normal normal 12px/14px Georgia;
+  letter-spacing: 0px;
+  color: #000000;
+  opacity: 0.5;
+`
+const Input = styled.input`
+  width: 100%;
+  height: 22px;
+  text-align: left;
+  font: normal normal normal 20px/22px Georgia;
+  letter-spacing: 2px;
+  color: #000000;
+  opacity: 1;
+`
+const BtnLogin = styled.img`
+    width: 135px;
+  height: 30px;
+  position: absolute;
+  bottom: 100px;
+  left: 97px;
+`
 export default  index
