5f7636c37d0725646bafe46c893ecaa68bcfe9c2
cu <hjkghjk>
2021-05-17 17:05:54 +0700

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
 import React, {  Component } from 'react';
-import Login from "./modules/Login/component/SignIn";
-import Register from "./modules/Login/component/SignUp";
-import RegisterOtp from "./modules/Login/component/SignUpOtp"
-import Dashboard from "./modules/dashboard/index";
-import Index from "../src/modules/index";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from "react-router-dom";
 import { observer } from 'mobx-react';
+import ListRouter from "../src/router/index";
 @observer
 export default class App extends Component<any,any> {
   render(){
     return (
-        // <Router  ref={instance  => {console.log(instance)} }>
-        //     <Switch>
-        //     <Route path="/dashboard" component={Dashboard}/>
-        //     <Route path = "/register-otp" component  = {RegisterOtp}/>
-        //     <Route path = "/register" component  = {Register}/>
-
-        //     <Route path="/" component = {Login}/>
-        //     </Switch>
-        // </Router>
-<Index/>
+        <React.Fragment>
+          <ListRouter/>
+        </React.Fragment>
 
     )
   }
