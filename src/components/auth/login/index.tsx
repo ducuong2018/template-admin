3151f627bb074d6b20cf3ac1d727d3e089961f39
cu <hjkghjk>
2021-05-20 08:05:28 +0700

    no message

--- src/components/auth/login/index.tsx
+++ src/components/auth/login/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import "./style.scss"
+import "./style.css"
 export default class index extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <div className = "formLogin">
-                    <input type = "text" placeholder = "Email"/>
-                    <input type = "password" placeholder = "Mật khẩu"/>
-                    <button>Đăng nhập</button>  
+                <div className = "container">
+                    <input className = "container_input" type = "text" placeholder = "Email"/>
+                    <input className = "container_input" type = "password" placeholder = "Mật khẩu"/>
+                    <button className = "container_button">Đăng nhập</button>  
                 </div>       
             </React.Fragment>
         )
